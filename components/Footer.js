249cc573385cacc64bd230bd4bc6b52556af226b
rahulrawat <rahulrawatwork@gmail.com>
2022-12-02 11:31:09 +0530

    feat: :sparkles: made responsive navbar

--- components/Footer.js
+++ components/Footer.js
@@ -5,18 +5,18 @@ import { TiSocialTwitter, TiSocialFacebook } from "react-icons/ti";
 function Footer() {
   return (
     <footer>
-        <div className='social-icons'>
+        <div className='social_icons'>
             <SlSocialInstagram size={30}/>
             <SlSocialSpotify size={30}/>
             <SlSocialYoutube size={30}/>
             <TiSocialTwitter size={30}/>
             <TiSocialFacebook size={30}/>
         </div>
-        <div className='footer-list'>
-            <ul >
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>FAQs</li>
+        <div >
+            <ul className='footer_list'>
+                <li className='footer_list_item'>About Us</li>
+                <li className='footer_list_item'>Contact Us</li>
+                <li className='footer_list_item'>FAQs</li>
             </ul>
         </div>
         <div className='footer-text'>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Link from 'next/link';
 
-import {GiMountains} from "react-icons/gi";
+import {GiMountains, GiHamburgerMenu} from "react-icons/gi";
 
 const Navbar = () => {
+  const navList = useRef();
+  const [navBarVisible, setNavBarVisible] = useState(false)
+  const showNavBar = ()=>{
+    if(navBarVisible){
+      navList.current.classList.remove('show_nav');
+    }else{
+      navList.current.classList.add('show_nav')
+    }
+    setNavBarVisible(!navBarVisible)
+  }
+
   return (
     <nav className='navbar'>
        <GiMountains size={50}/> 
-       <ul>
-          <li>
+       <ul className='nav_list' ref={navList}>
+          <li className='nav_item'>
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className='nav_item'>
             <Link href="/aboutUs">About Us</Link>
           </li>
-          <li>
+          <li className='nav_item'>
             <Link href="/">Blog</Link>
           </li>
        </ul>
+       <button className='nav_button'>
+          <GiHamburgerMenu size={25} onClick={()=>showNavBar()}/>
+       </button>
     </nav>
   )
 }
